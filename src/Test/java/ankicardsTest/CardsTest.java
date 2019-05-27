9370c28b00a9ed4e0c04d564daa17d52df311599
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-27 17:17:51 +0530

    Remove.the test case of giving the content of card

--- src/Test/java/ankicardsTest/CardsTest.java
+++ src/Test/java/ankicardsTest/CardsTest.java
@@ -14,11 +14,7 @@ public class CardsTest {
        String expected="Maths";
        assertEquals(expected,Maths.name);
    }
-   @Test public void itShouldGiveCardContent(){
-       Card content= new Card("what is object","instance of a class");
-       String expected = "what is object,instance of a class";
-       assertEquals(expected,content.toString());
-   }
+
    @Test public void cardShouldBeAddInDeck(){
        Deck programming=new Deck("programming");
        List<Card> cards=new ArrayList<>();
